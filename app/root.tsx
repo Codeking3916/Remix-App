35dc64104337e490f270b65689f64b5ff722a9b0
ck-siddharth <siddharth.s@codeking.in>
2024-08-05 16:56:02 +0530

    THis is my first code

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,35 @@
 import {
   Form,
+  Outlet,
   Links,
   Meta,
+  Link,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { LinksFunction, json  } from "@remix-run/node";
+import styleSheet from "./app.css";
+import { useLoaderData } from "@remix-run/react";
+import { getContacts } from "./data";
+
+export const links: LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: styleSheet,
+  },
+];
+
+export const loader = async () => {
+  const contacts = await getContacts();
+  return json({ contacts });
+};
 
 export default function App() {
+
+   const loaderData = useLoaderData<typeof loader>();
+   const  {contacts} = loaderData;
+  console.log(loaderData);
+
   return (
     <html lang="en">
       <head>
@@ -16,6 +39,7 @@ export default function App() {
         <Links />
       </head>
       <body>
+        
         <div id="sidebar">
           <h1>Remix Contacts</h1>
           <div>
@@ -34,15 +58,34 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+          {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
+        </div>
+        <div id="details">
+            <Outlet/>
         </div>
 
         <ScrollRestoration />
---
+++ app/routes/contacts.tsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+// import { Outlet } from '@remix-run/react'
+// export default function contact() {
+//   return (
+//     <>
+//      <div>contact</div>
+//      <Outlet/>
+//     </>
+   
+//   )
+// }
